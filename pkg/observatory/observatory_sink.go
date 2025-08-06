a4a37569b3cfa5355f95fd967245551ee45e156f
Daniil Kolesnichenko <garlicbreadcleric@gmail.com>
2025-08-06 20:27:23 +0400

    Batch observatory sink messages

--- pkg/observatory/observatory_sink.go
+++ pkg/observatory/observatory_sink.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/simple-container-com/go-aws-lambda-sdk/pkg/logger"
 )
@@ -12,35 +14,66 @@ import (
 type ObservatorySink struct {
 	baseUri string
 	apiKey  string
+
+	bufferSize int
+	flushTimer *time.Timer
+	flushDelay time.Duration
+	buffer     []logger.Message
+	mutex      sync.Mutex
 }
 
-func NewObservatorySink(baseUri string, apiKey string) *ObservatorySink {
+func NewObservatorySink(baseUri string, apiKey string, bufferSize int, flushDelay time.Duration) *ObservatorySink {
 	return &ObservatorySink{
-		baseUri: baseUri,
-		apiKey:  apiKey,
+		baseUri:    baseUri,
+		apiKey:     apiKey,
+		bufferSize: bufferSize,
+		flushDelay: flushDelay,
 	}
 }
 
 func (s *ObservatorySink) Write(msg logger.Message) error {
-	var logLevel int
-	switch msg.Level {
-	case logger.Error:
-		logLevel = 1
-	case logger.Warn:
-		logLevel = 2
-	case logger.Info:
-		logLevel = 3
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.buffer = append(s.buffer, msg)
+
+	// Reset flush timer
+	if s.flushTimer != nil {
+		s.flushTimer.Stop()
 	}
+	s.flushTimer = time.AfterFunc(s.flushDelay, func() {
+		s.Flush()
+	})
+
+	// Flush if buffer is full
+	if len(s.buffer) >= s.bufferSize {
+		return s.flush()
+	}
+
+	return nil
+}
+
+func (s *ObservatorySink) WriteImmediately(msgs []logger.Message) error {
 	body := ObservatoryPushLogsRequest{
-		Logs: []ObservatoryPushLogsRequestLog{
-			{
-				Message:   msg.Message,
-				LogLevel:  logLevel,
-				Data:      msg.Context,
-				Module:    nil,
-				Submodule: nil,
-			},
-		},
+		Logs: make([]ObservatoryPushLogsRequestLog, len(msgs)),
+	}
+	for i, msg := range msgs {
+		var logLevel int
+		switch msg.Level {
+		case logger.Error:
+			logLevel = 1
+		case logger.Warn:
+			logLevel = 2
+		case logger.Info:
+			logLevel = 3
+		}
+		body.Logs[i] = ObservatoryPushLogsRequestLog{
+			Message:   msg.Message,
+			LogLevel:  logLevel,
+			Data:      msg.Context,
+			Module:    nil,
+			Submodule: nil,
+		}
 	}
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
@@ -66,6 +99,32 @@ func (s *ObservatorySink) Write(msg logger.Message) error {
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("observatory log request failed with status: %s", resp.Status)
 	}
+	return nil
+}
+
+func (s *ObservatorySink) Flush() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	return s.flush()
+}
+
+func (s *ObservatorySink) flush() error {
+	if len(s.buffer) == 0 {
+		return nil
+	}
+
+	if err := s.WriteImmediately(s.buffer); err != nil {
+		return fmt.Errorf("failed to write buffered messages: %w", err)
+	}
+
+	// Clear buffer
+	s.buffer = s.buffer[:0]
+
+	if s.flushTimer != nil {
+		s.flushTimer.Stop()
+		s.flushTimer = nil
+	}
+
 	return nil
 }
 
