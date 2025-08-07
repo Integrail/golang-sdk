c8c97e664a78ca93a25f7d22090ffbada07719c1
Daniil Kolesnichenko <garlicbreadcleric@gmail.com>
2025-08-07 16:57:33 +0400

    Add WithExecutionCtx helper

---
+++ pkg/observatory/execution_ctx.go
@@ -0,0 +1,22 @@
+package observatory
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/simple-container-com/go-aws-lambda-sdk/pkg/logger"
+)
+
+func WithExecutionCtx(l logger.Logger, ctx context.Context, req http.Request) context.Context {
+	executionCtxJson := req.Header.Get("X-Everworker-Execution-Context")
+	if executionCtxJson == "" {
+		return ctx
+	}
+	var executionCtx map[string]any
+	if err := json.Unmarshal([]byte(executionCtxJson), &executionCtx); err != nil {
+		l.Errorf(ctx, "Failed to unmarshal execution context: %v", err)
+		return ctx
+	}
+	return context.WithValue(ctx, "executionCtx", executionCtx)
+}
